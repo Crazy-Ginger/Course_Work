44fecf9380ed352fda500dc0633236ea3dcce799
Matthew Wardle <mattydwardle@outlook.com>
2017-10-24 16:39:59 +0100

    Added a new module to try and autolog out on startup. Doesn't work yet.

---
+++ Course Work/Auto-log out.vb
@@ -0,0 +1,9 @@
+﻿Imports System.Data.OleDb
+Module Auto_log_out
+    Dim log As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|Datadirectory|User details.accdb")
+    log.Open()
+    Dim logged_query As String = "SELECT Logged in? FROM users"
+    Dim logger As New OleDbCommand(logged_query, log)
+    Dim logged_writer As OleDbDataReader = logger.
+
+End Module
