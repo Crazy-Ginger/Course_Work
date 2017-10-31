eee78025ec64ee92ff944d287bfc3f50a51c62c1
Matthew Wardle <mattydwardle@outlook.com>
2017-10-31 13:21:32 +0000

    Attempted to fix login but to no avail. It seems that DataDirectory will only work in ASP.net code as the vb seems to be looking in the wrong place.

--- Course Work/Login.aspx.vb
+++ Course Work/Login.aspx.vb
@@ -3,19 +3,24 @@ Public Class Login
     Inherits System.Web.UI.Page
     Public dataQueries As New DataSet1TableAdapters.TableAdapter
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        Dim Login As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|Datadirectory|User details.accdb")
+        AppDomain.CurrentDomain.SetData("DataDirectory", "\App_Data")
+        'Dim Login As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|Datadirectory|/User details.accdb")
+        'Login.Open()
+        'Dim username_query As String = "SELECT username FROM users"
+        'Dim password_query As String = "SELECT password from users"
+        'Dim usernames As New OleDbCommand(username_query, Login)
+        'Dim passwords As New OleDbCommand(password_query, Login)
+        'Dim reader_pass As OleDbDataReader = passwords.ExecuteReader()
+        'Dim reader_user As OleDbDataReader = usernames.ExecuteReader()
+        Dim Login As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\User details.accdb")
         Login.Open()
-        Dim username_query As String = "SELECT username FROM users"
-        Dim password_query As String = "SELECT password from users"
-        Dim usernames As New OleDbCommand(username_query, Login)
-        Dim passwords As New OleDbCommand(password_query, Login)
-        Dim reader_pass As OleDbDataReader = passwords.ExecuteReader()
-        Dim reader_user As OleDbDataReader = usernames.ExecuteReader()
-
+        Dim commandstring As New OleDbCommand("SELECT Username FROM users WHERE ID=3", Login)
+        Dim reader As OleDbDataReader = (commandstring.ExecuteReader())
+        TextBox1.Text = (reader).ToString
     End Sub
 
     Protected Sub B_Login_Click(sender As Object, e As EventArgs) Handles B_Login.Click
-        Dim Login As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|Datadirectory|User details.accdb")
+        Dim Login As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\User details.accdb")
         Login.Open()
         Dim commandstring As New OleDbCommand("SELECT Username FROM users WHERE ID=3", Login)
         Dim reader As OleDbDataReader = (commandstring.ExecuteReader())
@@ -24,12 +29,14 @@ Public Class Login
         logincommand.Parameters.AddWithValue("@username", Tb_username)
         logincommand.Parameters.AddWithValue("@password", Tb_password)
         Dim check As Integer = 0
-        'check = Val(logincommand.ExecuteScalar())
-        'If check > 0 Then
-        ' Session("logged in") = True
-        ' Server.Transfer("Weekly Summary.aspx", True)
-        ' Else
-        ' L_Incorrect.Visible = True
-        ' End If
+        check = Val(logincommand.ExecuteScalar())
+        If check > 0 Then
+            Session("logged in") = True
+            L_Incorrect.Text = "Logged in"
+            L_Incorrect.Visible = True
+            'Server.Transfer("Weekly Summary.aspx", True)
+        Else
+            L_Incorrect.Visible = True
+        End If
     End Sub
 End Class
