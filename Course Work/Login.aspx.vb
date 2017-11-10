6a91538fc479e54f232e58778b5831765a3a738b
School Computer <mattydwardle@outlook.com>
2017-11-10 09:54:50 +0000

    Changed the relationship of the database. Added commented out code that can change the |DataDirectory| path. Tried using non-relational file paths to no avail.

--- Course Work/Login.aspx.vb
+++ Course Work/Login.aspx.vb
@@ -3,23 +3,20 @@ Public Class Login
     Inherits System.Web.UI.Page
     Public dataQueries As New DataSet1TableAdapters.TableAdapter
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        'Dim Login As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|Datadirectory|/User details.accdb")
-        'Login.Open()
-        'Dim username_query As String = "SELECT username FROM users"
-        'Dim password_query As String = "SELECT password from users"
-        'Dim usernames As New OleDbCommand(username_query, Login)
-        'Dim passwords As New OleDbCommand(password_query, Login)
-        'Dim reader_pass As OleDbDataReader = passwords.ExecuteReader()
-        'Dim reader_user As OleDbDataReader = usernames.ExecuteReader()
-        Dim Login As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|User details.accdb")
+        'Dim executable As String = System.Reflection.Assembly.GetExecutingAssembly().Location
+        'Dim datapath As String = (System.IO.Path.GetDirectoryName(executable))
+        'AppDomain.CurrentDomain.SetData("DataDirectory", datapath)
+        ' Dim Login As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|User details.accdb")
+        Dim Login As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=W:\6th Form\Year 2\Computer Science\Course_Work\Course Work\App_Data\User details.accdb")
         Login.Open()
-        Dim commandstring As New OleDbCommand("SELECT Username FROM users WHERE ID=3", Login)
+        Dim commandstring As New OleDbCommand("SELECT Username FROM users", Login)
         Dim reader As OleDbDataReader = (commandstring.ExecuteReader())
         TextBox1.Text = (reader).ToString
     End Sub
 
     Protected Sub B_Login_Click(sender As Object, e As EventArgs) Handles B_Login.Click
-        Dim Login As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|User details.accdb")
+        'Dim Login As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|User details.accdb")
+        Dim Login As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=W:\6th Form\Year 2\Computer Science\Course_Work\Course Work\App_Data\User details.accdb")
         Login.Open()
         Dim commandstring As New OleDbCommand("SELECT Username FROM users WHERE ID=3", Login)
         Dim reader As OleDbDataReader = (commandstring.ExecuteReader())
