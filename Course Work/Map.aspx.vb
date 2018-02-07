43d50914d6607abdc9e22ae5be4bb78960020cfb
School Computer <mattydwardle@outlook.com>
2018-02-07 12:00:02 +0000

    Added textboxes for destinations

--- Course Work/Map.aspx.vb
+++ Course Work/Map.aspx.vb
@@ -2,52 +2,28 @@
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-
+        If boxNum <> 0 Then
+            For i As Integer = 0 To (boxNum)
+                Dim testBox As New TextBox
+                P_Dest_cont.Controls.Add(testBox)
+            Next
+        End If
     End Sub
+
     Protected Sub AddDestination_Click(sender As Object, e As EventArgs) Handles AddDestination.Click
-
-
-
-        'Dim Destination As New Textboxmaker
-        'Destination.ID = "Destination"
-
-        ''suffix += 1
-        'Me.Controls.Add(Destination)
-        'Destination.Visible = True
-        'Destination.
-        Dim rowsCount As Integer = CInt(Tx_Destinations.Text)
-        GenerateTable(rowsCount)
-    End Sub
-
-    'Public Class Textboxmaker
-    '    Inherits TextBox
-    '    Public ID As String
-    'End Class
-
-    Private Sub GenerateTable(ByVal rowsCount As Integer)
-        'Creat the Table and Add it to the Page
-        Dim table As New Table()
-        table.ID = "Table1"
-        Page.Form.Controls.Add(table)
-        ' Now iterate through the table and add your controls 
-        For i As Integer = 0 To rowsCount - 1
-            Dim row As New TableRow()
-
-            Dim cell As New TableCell()
-            Dim tb As New TextBox()
-
-            ' Set a unique ID for each TextBox added
-            tb.ID = "TextBoxRow_" & i
-
-            ' Add the control to the TableCell
-            cell.Controls.Add(tb)
-            ' Add the TableCell to the TableRow
-            row.Cells.Add(cell)
-            table.Rows.Add(row)
-        Next
-        ' Add the TableRow to the Table
-
-
+        boxNum += 1
     End Sub
 End Class
+
+Public Module globalVariables
+    Private priBoxNum As Int32 = 0
+    Property boxNum As Int32
+        Get
+            Return priBoxNum
+        End Get
+        Set(value As Int32)
+            priBoxNum = value
+        End Set
+    End Property
+End Module
 
