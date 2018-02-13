b152011c02cc6498b76610c64e9e473c81d83750
School Computer <mattydwardle@outlook.com>
2018-02-13 11:21:03 +0000

    Beginning to add brute force to solution

--- Course Work/travelingSalesman.vb
+++ Course Work/travelingSalesman.vb
@@ -21,7 +21,22 @@
     End Function
 End Class
 
+Sub main()
+    Dim nodes As New list(Of Integer)
+    Dim numNodes As Integer
+    numNodes = console.readline()
+    For i As Integer = 1 To numNodes
+        nodes.add(i)
+    Next
+    Permutation(nodes, numNodes)
+End Sub
 Public Structure Node
     Dim X As Int32
     Dim Y As Int32
 End Structure
+
+Public Function Permutation(ByVal nodes As list(Of Integer), ByVal low As Integer = 0)
+    If low + 1 >= nodes.Count Then
+
+    End If
+End Function
