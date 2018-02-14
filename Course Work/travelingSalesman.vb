624add4fe079df83918066dd7d3122f27301ed64
Matthew Wardle <mattydwardle@outlook.com>
2018-02-14 20:52:00 +0000

    Changed TSP

--- Course Work/travelingSalesman.vb
+++ Course Work/travelingSalesman.vb
@@ -1,7 +1,7 @@
-﻿Public Sub Start()
+﻿Public Function Start()
     Dim nodes As New List(Of Integer)
-    Dim numNodes As Integer
-    numNodes = Console.ReadLine()
+Dim numNodes As Integer
+numNodes = Console.ReadLine()
     For i As Integer = 1 To numNodes
         nodes.Add(i)
     Next
