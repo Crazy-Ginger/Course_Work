9b2cd1f58e0971e955f9a45dfdabc4f7653a1e1f
Matthew Wardle <mattydwardle@gmail.com>
2018-03-29 00:01:13 +0100

    Further modifications Hopefully still works

--- Course Work/Login.aspx.vb
+++ Course Work/Login.aspx.vb
@@ -4,22 +4,21 @@
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         Dim logged_In As Boolean = Session("logged_in")
         l_Incorrect.Visible = True
-        If logged_In = True Then
+        If Session("logged_in") = True Then
             MsgBox("You are already logged in. Redirecting to Map")
-            Server.Transfer("Stopover.aspx") 'use response.readirect to remove this page from the memory
+            Response.Redirect("Stopover.aspx") 'use response.readirect to remove this page from the memory
         End If
     End Sub
 
     Protected Sub B_Login_Click(sender As Object, e As EventArgs) Handles b_Login.Click
 
-        If Login_Connection.Login(tb_username.Text, tb_password.Text) Is Nothing Or Login_Connection.Login(tb_username.Text, tb_password.Text) < 0 Then
+        If Login_Connection.Login(tb_username.Text, tb_password.Text) Is Nothing Or Login_Connection.Login(tb_username.Text, tb_password.Text) <= 0 Then
             l_Incorrect.Visible = True
-        Else
+        ElseIf Login_Connection.Login(tb_username.Text, tb_password.Text) >= 1 Then
             Session("username") = tb_username.Text
             Session("logged_in") = True
             MsgBox("You are now logged in")
             Server.Transfer("Map.aspx", True)
-
         End If
     End Sub
 End Class
