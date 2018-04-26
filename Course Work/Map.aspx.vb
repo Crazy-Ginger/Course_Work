4dcb1809329ec2e864ef7aff0b400193b5fc6673
Matthew Wardle <mattydwardle@gmail.com>
2018-04-27 00:34:58 +0100

    Commented code to make it easier to read and screenshot Also renamed Persistance to Persistence (correct spelling) Consider using "Gettype" to fix RFV problem

--- Course Work/Map.aspx.vb
+++ Course Work/Map.aspx.vb
@@ -13,22 +13,26 @@ Public Class Shorter
 End Class
 
 
-Public Module Persistance
+Public Module Persistence
     Public Destinations As TextBox() = {New TextBox}
-    Public rfv As RequiredFieldValidator() = {New RequiredFieldValidator}
 End Module
 
+'Public rfv As RequiredFieldValidator() = {New RequiredFieldValidator}
 Public Class Map
     Inherits System.Web.UI.Page
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         p_routenodes.Controls.Clear()
-        Dim script As String = ""
-        For Each tb As TextBox In Persistance.Destinations
-            p_routenodes.Controls.Add(tb)
-            script = "<script> </script>"
 
+        'adds the text boxes stored in "Persistence" to the panel
+        For Each tb As TextBox In Persistence.Destinations
+            p_routenodes.Controls.Add(tb)
         Next
+
+        'Dim script As String = ""
+        'script = "<script> </script>"
         Page.ClientScript.RegisterClientScriptInclude("Map_Scripts.js", "~/Scripts/Map_Scripts.js")
+
+        'consider using GetType
         'For Each rfv As RequiredFieldValidator In Persistance.rfv
         '    p_routenodes.Controls.Add(rfv)
         'Next
@@ -49,11 +53,17 @@ Public Class Map
 
     Protected Sub Page_Unload(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Unload
         Dim tb_list As New List(Of TextBox)
+
         'Dim rfv_list As New List(Of RequiredFieldValidator)
+
+        'adds the textboxes in the panel to the list
         For Each tb As TextBox In p_routenodes.Controls
             tb_list.Add(tb)
-            Persistance.Destinations = tb_list.ToArray()
         Next
+
+        'sets the value of the remote variable so that the textboxes are preservered between page loads
+        Persistence.Destinations = tb_list.ToArray()
+
         'For Each rfv As RequiredFieldValidator In p_routenodes.Controls
         '    rfv_list.Add(rfv)
         '    Persistance.rfv = rfv_list.ToArray()
@@ -62,16 +72,20 @@ Public Class Map
 
     Protected Sub AddDestination_Click(sender As Object, e As EventArgs) Handles b_AddDestination.Click
         Dim tb As New TextBox()
+
         'Dim rfv As New RequiredFieldValidator()
-        Dim id As String = "tb_waypoints" & Persistance.Destinations.Length
+
+        Dim id As String = "tb_waypoints" & Persistence.Destinations.Length
         tb.ID = id
+
         'rfv.ControlToValidate = id
         'p_routenodes.Controls.Add(rfv)
+
         p_routenodes.Controls.Add(tb)
     End Sub
 
     Protected Sub RemoveDestination_click(sender As Object, e As EventArgs) Handles b_LessDestination.Click
-        Dim tb As TextBox = Persistance.Destinations(Persistance.Destinations.Length - 1)
+        Dim tb As TextBox = Persistence.Destinations(Persistence.Destinations.Length - 1)
         'tb.ID = "tb_wapoints" & Persistance.Destinations.Length
         p_routenodes.Controls.Remove(tb)
         'Dim rfv As RequiredFieldValidator = Persistance.rfv(Persistance.rfv.Length - 1)
